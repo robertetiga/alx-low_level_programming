a20140dc3c1f71e1df4ff24f2ff57b9c1c20d41f
Robert Etiga <agidediaetiga@gmail.com>
2022-06-03 09:14:42 -0700

    Modify 1-last_digit.c executable file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,20 +8,20 @@
  */
 int main(void)
 {
-	int n, lastdigit;
+	int n, lastdgt;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastdigit = n % 10;
-	if (lastdigit > 5)
+	lastdgt = n % 10;
+	if (lastdgt > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
-	} else if (lastdigit == 0)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastdgt);
+	} else if (lastdgt == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastdgt);
 	} else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, lastdigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdgt);
 	}
 	return (0);
 }
