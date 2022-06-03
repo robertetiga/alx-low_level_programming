31f7e7b70802a915fc9efe52aadc3ab1974af360
Robert Etiga <agidediaetiga@gmail.com>
2022-06-03 15:58:56 -0700

    Create 5-print_numbers.c executable file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10 starting from 0
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
