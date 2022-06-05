6de269237bcf456de7fdcf2e97679ecea5d8ea07
Agidedia Robert Etiga <agidediaetiga@gmail.com>
2022-06-05 12:16:09 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,42 +6,42 @@
  */
 int main(void)
 {
-	int a, b, c, d, opp1, opp2;
+int a, b, c, d, opp1, opp2;
 
-	a = b = c = d = 48;
-	while (d < 58)
+a = b = c = d = 48;
+while (d < 58)
+{
+	c = 48;
+	while (c < 58)
 	{
-		c = 48;
-		while (c < 58)
+		b = 48;
+		while (b < 58)
 		{
-			b = 48;
-			while (b < 58)
+			a = 48;
+			while (a < 58)
 			{
-				a = 48;
-				while (a < 58)
+				opp1 = (d * 10) + c;
+				opp2 = (b * 10) + a;
+				if (opp1 < opp2)
 				{
-					opp1 = (d * 10) + c;
-					opp2 = (b * 10) + a;
-					if (opp1 < opp2)
-					{
-						putchar(d);
-						putchar(c);
-						putchar(' ');
-						putchar(b);
-						putchar(a);
-						if (d == 57 && c == 56 && b == 57 && a == 57)
-							break;
-						putchar(',');
-						putchar(' ');
-					}
-					a++;
+					putchar(d);
+					putchar(c);
+					putchar(' ');
+					putchar(b);
+					putchar(a);
+					if (d == 57 && c == 56 && b == 57 && a == 57)
+						break;
+					putchar(',');
+					putchar(' ');
 				}
-				b++;
+				a++;
 			}
-			c++;
+			b++;
 		}
-		d++;
+		c++;
 	}
-	putchar('\n');
-	return (0);
+	d++;
+}
+putchar('\n');
+return (0);
 }
