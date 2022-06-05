eb97b30f8a45a3ff55ab7bc8018e40d3edc662c6
Robert Etiga <agidediaetiga@gmail.com>
2022-06-05 04:06:32 -0700

    Create 100-print_comb3.c executable file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of 00 to 89
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+
+	while (j < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (j != i && j < i)
+			{
+				putchar(j);
+				putchar(i);
+				if (i == 57 && j == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
