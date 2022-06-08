19bf41fe029043173ff870bcc8bc54d3b19d62c1
Robert Etiga <agidediaetiga@gmail.com>
2022-06-07 21:06:19 -0700

    Create 10-add.c program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - adds two integers and returns the result
+*
+* @num1: integer 1
+* @num2: integer 2
+*
+* Return: sum of integers
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
