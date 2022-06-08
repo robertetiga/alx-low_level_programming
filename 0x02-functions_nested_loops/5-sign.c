ce4ec7b363652bab25fb73d8902d5454ca6fd969
Robert Etiga <agidediaetiga@gmail.com>
2022-06-07 19:28:10 -0700

    Create 5-sign.c program

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* print_sign - determines if the input is greater
+* than zero, is zero, or less than zero
+*
+* @n: input is integer
+*
+* Return: 1 if greater than 0, 0 if 0,
+* -1 if less than 0
+*/
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 1)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
