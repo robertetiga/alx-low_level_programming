970db15676883b1720c7941d1beb6ef051a8f605
Robert Etiga <agidediaetiga@gmail.com>
2022-06-08 16:01:41 -0700

    Create 103-fibonacci.c program

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - finds and prints the sum of the even-valued terms
+ * of the fibonacci sequence whose values do not exceed
+ * 4,000,000
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	long int num1, num2, fib, fibSum;
+
+	num1 = 1;
+	num2 = 2;
+	fib = fibSum = 0;
+	while (fib <= 4000000)
+	{
+		fib = num1 + num2;
+		num2 = num2;
+		num2 = fib;
+		if ((num1 % 2) == 0)
+		{
+			fibSum += num1;
+		}
+	}
+	printf("%ld\n", fibSum);
+	return (0);
+}
