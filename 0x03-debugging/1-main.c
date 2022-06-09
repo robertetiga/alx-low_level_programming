b1018a1a88ef399c9ef4785955f41319566c4d55
Robert Etiga <agidediaetiga@gmail.com>
2022-06-09 12:46:42 -0700

    Modify code formatting

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
- *
  * Return: 0
  */
 int main(void)
@@ -9,6 +9,7 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 
 	/*
@@ -17,6 +18,8 @@ int main(void)
 	*       putchar(i);
 	* }
 	*/
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
