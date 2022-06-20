3a96f4ebc0ae3663a5954d68b88d69f34ba30470
Robert Etiga <agidediaetiga@gmail.com>
2022-06-20 15:09:37 -0700

    Created func that prints a chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,17 +2,17 @@
 
 /**
  * print_chessboard - prints a chessboard
- * @: the chessboard to be printed
+ * @a: the chessboard to be printed
  */
-
 void print_chessboard(char (*a)[8])
 {
-	unsigned int arr1, arr2;
+	int arr1, arr2;
 
 	for (arr1 = 0; a[arr1][7]; arr1++)
 	{
 		for (arr2 = 0; arr2 < 8; arr2++)
 			_putchar(a[arr1][arr2]);
+
 		_putchar('\n');
 	}
 }
