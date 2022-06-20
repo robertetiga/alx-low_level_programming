b40dad51c793484e0f9897e9b33af74024549ca5
Robert Etiga <agidediaetiga@gmail.com>
2022-06-20 13:52:19 -0700

    Created func that locates a char in a str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to perform operation on
+ * @c: character to locate
+ * Return: the pointer to the first occurence
+ * of the character c
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
