4094464e8def83ca096c09390419f151fc64a221
Robert Etiga <agidediaetiga@gmail.com>
2022-06-24 17:27:18 -0700

    Correct variable declaration

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,9 +13,7 @@ void print_buffer(char *b, int size)
 	int j, k, l;
 
 	if (size <= 0)
-	{
 		printf("\n");
-	}
 	else
 	{
 		for (j = 0; j < size; j += 10)
@@ -35,10 +33,10 @@ void print_buffer(char *b, int size)
 			{
 				if (l >= size)
 					break;
-				if (*(b + 1) < 32 || *(b + 1) > 126)
+				if (*(b + l) < 32 || *(b + l) > 126)
 					printf("%c", '.');
 				else
-					printf("%c", *(b + 1));
+					printf("%c", *(b + l));
 			}
 			printf("\n");
 		}
